867f89de33896a0af2705d47c7ba3a0c4fec8ca7
karloid <krldboyan@gmail.com>
2014-09-08 02:35:12 +0400

    Initial commit

---
+++ src/main/java/MyStrategy.java
@@ -0,0 +1,13 @@
+import model.*;
+
+import static java.lang.StrictMath.PI;
+
+public final class MyStrategy implements Strategy {
+    @Override
+    public void move(Hockeyist self, World world, Game game, Move move) {
+        move.setSpeedUp(-1.0D);
+        move.setTurn(PI);
+        move.setAction(ActionType.STRIKE);
+        move.setAction(ActionType.PASS);
+    }
+}
